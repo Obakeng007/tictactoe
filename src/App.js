05dbc9d7c378b1a74a8f7df26477058b8567c944
Obakeng Dusani <obakeng.dusani@bothouniversity.ac.bw>
2022-02-02 22:33:39 +0200

    Setting up player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
-import './App.css';
+import Square from "./Components/Square"
+import { useState } from 'react';
+
+const boardstyling ={
+flexDirection: 'column',
+width: '500px',
+height: '500px',
+display: 'flex',
+backgroundColor: 'aquamarine',
+border: '1px solid black',
+}
+
+const boardBody ={
+  marginTop: '400px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  display: 'flex'
+}
+
+const row ={
+  display: 'flex',
+  border: '1px solid black',
+  flex: '33%',
+  flexDirection: 'row'
+}
+
+
 
 function App() {
+  const [board, setBoard] = useState(["","","","","","","","",""]);
+  const [isPlayer, setPlayer] = useState("X");
+
+  const clickSquare = (squareData) => {setBoard(
+    board.map((value,boardData) =>{
+      if (boardData == squareData && value == ""){
+        return isPlayer;
+      }
+      return value;
+    })
+  )};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={ boardBody }>
+      <div style={ boardstyling }>
+        <div style={ row } >
+          <Square value={ board[0] } selectSquare={ () => {clickSquare(0)} } />
+          <Square value={ board[1] } selectSquare={ () => {clickSquare(1)} }/>
+          <Square value={ board[2] } selectSquare={ () => {clickSquare(2)}} />
+        </div>
+        <div style={ row }>
+          <Square value={ board[3] } selectSquare={ () => {clickSquare(3)}} />
+          <Square value={ board[4] } selectSquare={ () => {clickSquare(4)}}/>
+          <Square value={ board[5] } selectSquare={ () => {clickSquare(5)}} />
+
+        </div>
+        <div style={ row }>
+          <Square value={ board[6] } selectSquare={ () => {clickSquare(6)}} />
+          <Square value={ board[7] } selectSquare={ () => {clickSquare(7)}} />
+          <Square value={ board[8] } selectSquare={ () => {clickSquare(8)}} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Square.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.js';
+
+const Squarestyling ={
+    flex: '33%',
+    border: '1px solid black',
+    cursor: 'pointer',
+    heght: '100%',
+    width: '100%',
+    display: 'grid',
+    placeItems: 'center'
+  }
+
+export function Square({value, selectSquare}){
+
+   return <div style={ Squarestyling } onClick={selectSquare}>
+        { value }
+    </div>
+
+}
+
+
+export default Square;
