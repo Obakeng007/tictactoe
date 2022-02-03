706c97cc2d25ffb1a5c1fc5be5ba5d1285c9db39
Obakeng Dusani <obakeng.dusani@bothouniversity.ac.bw>
2022-02-03 20:18:03 +0200

    Which player won

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import Square from "./Components/Square"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const boardstyling ={
 flexDirection: 'column',
@@ -30,40 +29,82 @@ const row ={
 function App() {
   const [board, setBoard] = useState(["","","","","","","","",""]);
   const [isPlayer, setPlayer] = useState("X");
+  const [results, setResults] = useState({winner: " none ", state: "none"});
+
+  useEffect(() => {
+    isWinner();
+    if (isPlayer === ("X")){
+      setPlayer("O")
+    }
+    else { (setPlayer("X"))}
+  }, [board]);
+
+  useEffect(() => {
+    if (results.state !== "none"){
+
+      alert('Game finished! Winner is player: ', results.winner)
+    }
+  }, [results]);
 
   const clickSquare = (squareData) => {setBoard(
     board.map((value,boardData) =>{
-      if (boardData == squareData && value == ""){
+      if (boardData === squareData && value === ""){
         return isPlayer;
       }
       return value;
     })
 
   )
-
-    if (isPlayer === ("X")){
-      setPlayer("O")
-    }
-    else { (setPlayer("X"))}
-
   };
 
- 
+  const pattern = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [1, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [2, 5, 8]
+  ];
+
+  const isWinner = () =>{
+    pattern.forEach((currentPattern) => {
+    const playerOne = board[currentPattern[0]];
+    if (playerOne === "") return;
+
+    let winningPattern = true
+    currentPattern.forEach((value) => {
+      if (board[value] !== playerOne){
+
+        winningPattern = false;
+      }
+    } 
+    );
+
+    if (winningPattern){
+      setResults({winner: isPlayer, state: "won"})
+    }
+    }
+    )
+  }
   
   return (
     <div style={ boardBody }>
       <div style={ boardstyling }>
+
         <div style={ row } >
-          <Square value={ board[0] } selectSquare={ () => {clickSquare(0)} } />
-          <Square value={ board[1] } selectSquare={ () => {clickSquare(1)} }/>
+          <Square value={ board[0] } selectSquare={ () => {clickSquare(0)}} />
+          <Square value={ board[1] } selectSquare={ () => {clickSquare(1)}}/>
           <Square value={ board[2] } selectSquare={ () => {clickSquare(2)}} />
         </div>
+
         <div style={ row }>
           <Square value={ board[3] } selectSquare={ () => {clickSquare(3)}} />
           <Square value={ board[4] } selectSquare={ () => {clickSquare(4)}}/>
           <Square value={ board[5] } selectSquare={ () => {clickSquare(5)}} />
-
         </div>
+        
         <div style={ row }>
           <Square value={ board[6] } selectSquare={ () => {clickSquare(6)}} />
           <Square value={ board[7] } selectSquare={ () => {clickSquare(7)}} />
